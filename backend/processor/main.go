65804dfb7c743eb5db164cbd38fbf586facdaa21
victor.dev <victor.vdt1@gmail.com>
2025-06-12 16:43:44 -0300

    feat: add mongodb

--- backend/processor/main.go
+++ backend/processor/main.go
@@ -5,7 +5,10 @@ import (
 	"encoding/json"
 	"log"
 	"news-recommender/m/processor/classifier"
+	"news-recommender/m/processor/db"
+	"news-recommender/m/processor/model"
 	"os"
+	"time"
 
 	redisstore "news-recommender/m/processor/redis"
 
@@ -18,6 +21,15 @@ func main() {
 	broker := os.Getenv("KAFKA_BROKER")
 	if broker == "" {
 		broker = "localhost:9093" // fallback para desenvolvimento local
+	}
+
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		mongoURI = "mongodb://localhost:27017"
+	}
+	err := db.ConectarMongo(mongoURI)
+	if err != nil {
+		log.Fatalf("❌ Erro ao conectar ao MongoDB: %v", err)
 	}
 
 	// 1. Inicializa o classificador
@@ -36,6 +48,8 @@ func main() {
 	}
 	storage := redisstore.NewRedisStorage(addr)
 
+	// 3. Loop de leitura de mensagens do Kafka
+	log.Println("🔄 Iniciando o processamento de notícias...")
 	for {
 		msg, err := reader.ReadMessage(context.Background())
 		if err != nil {
@@ -58,6 +72,7 @@ func main() {
 
 		news["category"] = category
 
+		// 5. Monta o objeto NewsItem para o Redis
 		newsItem := redisstore.NewsItem{
 			Title:       news["title"],
 			Description: news["description"],
@@ -65,11 +80,28 @@ func main() {
 			Link:        news["link"],
 		}
 
+		// 6. Salva no Redis
 		err = storage.Save(newsItem)
 		if err != nil {
 			log.Printf("Erro ao salvar no Redis: %v", err)
 		}
 
+		// 7. Monta o objeto Noticia para o Mongo
+		noticia := model.Noticia{
+			Titulo:    news["title"],
+			Link:      news["link"],
+			Categoria: category,
+			Fonte:     "G1",
+			CriadoEm:  time.Now(),
+		}
+
+		// 8. Salva no MongoDB
+		err = db.SalvarNoticia(noticia)
+		if err != nil {
+			log.Printf("Erro ao salvar no MongoDB: %v", err)
+		}
+
+		// 9. Log de sucesso
 		log.Printf("ClassificadoTest: %s → %s", news["title"], category)
 	}
 }
